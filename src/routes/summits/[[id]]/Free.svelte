fc53a37a8503e893c5233c5f6e776402f18e922c
dominika <dominik.albrecht@intersim.ch>
2025-01-14 16:03:29 +0100

    [chore] typo

--- src/routes/summits/[[id]]/Free.svelte
+++ src/routes/summits/[[id]]/Free.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex items-center gap-2 rounded-lg bg-base-100">
 	<img src={Check} alt="Trophy" class="h-12 w-12" />
-	<div class="font-bold">Pass frei!! Schnapp in dir!</div>
+	<div class="font-bold">Pass frei!! Schnapp ihn dir!</div>
 </div>
