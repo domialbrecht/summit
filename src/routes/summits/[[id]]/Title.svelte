a641f8d8c0aac777b68dcbb520c73addfa8c7b0a
dominika <dominik.albrecht@intersim.ch>
2025-02-22 18:23:49 +0100

    [fix] title alignment

--- src/routes/summits/[[id]]/Title.svelte
+++ src/routes/summits/[[id]]/Title.svelte
@@ -13,16 +13,8 @@
 	} = $props();
 </script>
 
-<div class="flex flex-col items-center justify-between gap-1 xl:flex-row">
-	<div class="flex shrink flex-col flex-wrap gap-2 xl:flex-row xl:gap-4">
-		<h1 class="font-title mb-0 text-[clamp(1.5rem,4vw,3rem)] font-black leading-none text-primary">
-			{summit_data.summit.name}
-		</h1>
-		{#if summit_data.summit.alias}
-			<span class="text-2xl font-bold text-slate-300">{summit_data.summit.alias}</span>
-		{/if}
-	</div>
-	<div class="flex shrink-0 items-center gap-2">
+<div class="flex flex-col justify-between gap-1 xl:flex-row">
+	<div class="flex shrink-0 items-center justify-end gap-2 xl:order-2">
 		<button
 			class="btn btn-ghost text-accent"
 			onclick={() => handleTarget(summit_data.summit.lat, summit_data.summit.long)}
@@ -42,6 +34,14 @@
 		<button class="btn btn-ghost" onclick={handleClick}>
 			<X class="h-6 w-6" />
 		</button>
+	</div>
+	<div class="flex shrink flex-col flex-wrap gap-2 xl:flex-row xl:gap-4">
+		<h1 class="font-title mb-0 text-[clamp(1.5rem,4vw,3rem)] font-black leading-none text-primary">
+			{summit_data.summit.name}
+		</h1>
+		{#if summit_data.summit.alias}
+			<span class="text-2xl font-bold text-slate-300">{summit_data.summit.alias}</span>
+		{/if}
 	</div>
 </div>
 <div class="mt-4 flex gap-2">
